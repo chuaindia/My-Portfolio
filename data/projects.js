b72bc184a3df62b6c00e50ece3ddb46c9334593d
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-03-22 16:58:54 +0530

    Add 2nd Project

---
+++ data/projects.js
@@ -0,0 +1,58 @@
+const projects = [
+    {
+      title: "BookMe",
+      description: "This is an app in wich you can reserve a tour, create a tour, delete a tour and show all your reserved tours. How this App works is like when you run the app you will see the screen with all available tours. You can see their details but to reserve a Tour you need to Login ( if not Signup need to do it before ). Then you can reserve any number of available tours and cancel the reservation anytime. You can also start your own bussiness by creating a tour and incase you dont want it after creating you can delete it but all the reservation will also be destroyed! ",
+      imageUrl: "./images/project_images/bookme.png",
+      technologies: ["Ruby on Rails", "React", "Redux", "JavaScript", "Ruby", "Swipper", "CryptoJs"],
+      liveVersion: "https://book-tours.netlify.app/",
+      source: "https://github.com/ABDULALI3468/bookMe-frontend",
+    },
+  
+    {
+      title: "Foodify capstone project",
+      description: "This is a website that showcases top-notch food and its recipes from all over the world from Britain to Australia, from veg to meat. It will take your response in the form of likes and comments.",
+      imageUrl: "./images/project_images/foodify_website.png",
+      technologies: ["MEALDB API", "JavaScript", "Webpack", "CSS", "Html", "ES6 modules"],
+      liveVersion: "https://abdulali3468.github.io/Foodify_capstone_project/",
+      source: "https://github.com/ABDULALI3468/Foodify_capstone_project",
+    },
+  
+    {
+      title: "Token World (Crypto)",
+      description: "A website that provides accurate data about various crypto currencies. It will show you everything you need to know. This includes marketcap, top and low sellings, its history and gave you some core advices you should know before taking any step.",
+      imageUrl: "./images/project_images/Cryptocurrency_project.webp",
+      technologies: ["React", "Redux", "Jest", "JavaScript", "CSS", "Html"],
+      liveVersion: "https://tokenworld.netlify.app",
+      source: "https://github.com/ABDULALI3468/crypto-capstone",
+    },
+  
+    {
+      title: "Budget Tracker",
+      description: "A classic budget app to keep track of your expenses by category, made with Rails, Turbo's Stimulus and CSS.",
+      imageUrl: "./images/project_images/budget-app.png",
+      technologies: ["Ruby on Rails", "Ruby", "Devise", "JavaScript", "CSS", "Html"],
+      liveVersion: "https://yours-budget-application.herokuapp.com/",
+      source: "https://github.com/ABDULALI3468/Budget-app",
+    },
+  
+    {
+      title: "Space Travellers' Hub",
+      description: "This is a web application that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.",
+      imageUrl: "./images/project_images/space_travellers_hub.jpg",
+      technologies: ["JavaScript", "Webpack", "CSS", "Html", "ES6 modules"],
+      liveVersion: "https://chuaindia.github.io/harry_tanusri_space_travelers_hub/",
+      source: "https://github.com/chuaindia/harry_tanusri_space_travelers_hub",
+    },
+  
+    {
+      title: "Math Magicians",
+      description: "In this project I am creating a Single Page App using React regarding basic Calculator and its functionalities.",
+      imageUrl: "./images/project_images/mathmagician.jpg",
+      technologies: ["React", "JavaScript", "CSS", "ES6 modules", "JSX"],
+      liveVersion: "https://chuaindia.github.io/React-First-Project/",
+      source: "https://github.com/ABDULALI3468/React-First-Project",
+    },
+  ];
+  
+  export default projects;
+  
